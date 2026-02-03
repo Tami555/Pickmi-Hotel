924e94aaad98c2435bac27ff5625aec7555033e0
Tamiron <tdrozdikova@gmail.com>
2026-02-03 20:26:32 +0300

    refactor: adding working links to Footer. Added links to the phone and VK

--- web-app/src/components/layouts/Footer.jsx
+++ web-app/src/components/layouts/Footer.jsx
@@ -7,9 +7,9 @@ export const Footer = () => {
         <div className="footer-block">
             <h2 className="title">Your Pickmi Hotel</h2>
             <div className="main-infa-block">
-                <p>Отель на ул.Пушкина 87</p>
-                <p>контакты: +79546837356</p>
-                <p>Вконтанте: @PicmiH</p>
+                <p className="item">Отель на ул.Пушкина 87</p>
+                <a className="item" href="tel:+79546837356">Контакты: +79546837356</a>
+                <a className="item" href="https://vk.com" target="_blank">Вконтанте: @PicmiH</a>
             </div>
             <p className="open-data">с 1867 до н.э-2026</p>
         </div>
